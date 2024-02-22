04c4f65eafbe3761ced38b4c8b6573fa044a0d9a
Krimothiazine <krimouv1995@gmail.com>
2024-02-22 20:54:01 +0100

    Create article cards

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const ArticleCard = ({ creationDate, title, content, articleId }) => {
+  return (
+    <div className="flex flex-col gap-3 rounded-lg border border-gray-200 p-4 shadow-lg">
+      <p>{creationDate}</p>
+      <h3 className="text-2xl font-bold">{title}</h3>
+      <p className="line-clamp-3">{content.replace(/(<([^>]+)>)/gi, "")}</p>
+      <div className="flex gap-4">
+        <Link
+          to={`/articles/${articleId}`}
+          className="rounded bg-black p-2 text-white"
+        >
+          See more
+        </Link>
+        <Link
+          to={`/articles/${articleId}/edit`}
+          className="rounded bg-black p-2 text-white"
+        >
+          Edit
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+ArticleCard.propTypes = {
+  creationDate: PropTypes.string,
+  title: PropTypes.string,
+  content: PropTypes.string,
+  articleId: PropTypes.string,
+};
+
+export default ArticleCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import { getArticles } from "../api/getArticles";
+import ArticleCard from "../components/ArticleCard";
 
 export const loader = async () => {
   return await getArticles();
@@ -8,11 +9,40 @@ export const loader = async () => {
 const Home = () => {
   const data = useLoaderData();
   console.log("data:", data);
+  const published = data.articles.filter((article) => article.published);
+  const unpublished = data.articles.filter((article) => !article.published);
 
   return (
     <main className="px-4 py-2 pt-4 md:px-40">
-      <div className="flex items-center gap-6 border-b border-gray-200 pb-4 opacity-70">
-        HOme
+      <div className="">
+        <h2 className="border-b border-gray-200 text-3xl font-bold">
+          Unpublished articles:
+        </h2>
+        <div className="my-4 grid grid-cols-1 gap-3 md:grid-cols-3">
+          {published.map((article) => (
+            <ArticleCard
+              key={article.id}
+              creationDate={article.creationDate}
+              title={article.title}
+              content={article.content}
+              articleId={article.id}
+            />
+          ))}
+        </div>
+        <h2 className="border-b border-gray-200 text-3xl font-bold">
+          Published articles:
+        </h2>
+        <div>
+          {unpublished.map((article) => (
+            <ArticleCard
+              key={article.id}
+              creationDate={article.creationDate}
+              title={article.title}
+              content={article.content}
+              articleId={article.id}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
