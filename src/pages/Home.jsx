a73acbaecfd260c8e078346ef9caba554dd3e5d5
Krimothiazine <krimouv1995@gmail.com>
2024-02-28 20:09:55 +0100

    Handle empty articles arrays

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,10 @@ export const loader = async () => {
 const Home = () => {
   const data = useLoaderData();
   console.log("data:", data);
-  const published = data.articles.filter((article) => !article.published);
-  const unpublished = data.articles.filter((article) => article.published);
+  const published =
+    data.articles?.filter((article) => !article.published) || [];
+  const unpublished =
+    data.articles?.filter((article) => article.published) || [];
 
   return (
     <main className="px-4 py-2 pt-4 md:px-40">
@@ -19,29 +21,41 @@ const Home = () => {
           Unpublished articles:
         </h2>
         <div className="my-4 grid grid-cols-1 gap-3 md:grid-cols-3">
-          {published.map((article) => (
-            <ArticleCard
-              key={article.id}
-              creationDate={article.creationDate}
-              title={article.title}
-              content={article.content}
-              articleId={article.id}
-            />
-          ))}
+          {published.length > 0 ? (
+            published.map((article) => (
+              <ArticleCard
+                key={article.id}
+                creationDate={article.creationDate}
+                title={article.title}
+                content={article.content}
+                articleId={article.id}
+              />
+            ))
+          ) : (
+            <p className="p-3 font-semibold text-red-600">
+              You must be logged in to see articles
+            </p>
+          )}
         </div>
         <h2 className="border-b border-gray-200 text-3xl font-bold">
           Published articles:
         </h2>
         <div className="my-4 grid grid-cols-1 gap-3 md:grid-cols-3">
-          {unpublished.map((article) => (
-            <ArticleCard
-              key={article.id}
-              creationDate={article.creationDate}
-              title={article.title}
-              content={article.content}
-              articleId={article.id}
-            />
-          ))}
+          {unpublished.length > 0 ? (
+            unpublished.map((article) => (
+              <ArticleCard
+                key={article.id}
+                creationDate={article.creationDate}
+                title={article.title}
+                content={article.content}
+                articleId={article.id}
+              />
+            ))
+          ) : (
+            <p className="p-3 font-semibold text-red-600">
+              You must be logged in to see articles
+            </p>
+          )}
         </div>
       </div>
     </main>
